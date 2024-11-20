116395ed8ba8f3a2c88c15a7e1215f81adf48c3a
Matt Marino <mttym42@icloud.com>
2024-11-20 08:29:21 -0500

    edit to misc script

--- misc.sh
+++ misc.sh
@@ -1,3 +1,5 @@
-#! Misc packages and apps
+#! /bin/bash
+
+# Misc packages and apps
 
 sudo apt install lolcat innoextract htop lame timeshift fortune-mod fortunes cowsay cowsay-off sl cmatrix cmatrix-xfont geany geany-plugins qcomicbook
