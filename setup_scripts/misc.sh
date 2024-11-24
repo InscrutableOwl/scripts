d9901323a571f261ca194d4ae0a44bb916befe4a
Matt Marino <mttym42@icloud.com>
2024-11-24 07:29:12 -0500

    arranging scripts into distro specific folders

--- setup_scripts/misc.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-# Misc packages and apps
-
-sudo apt install lolcat innoextract htop lame timeshift fortune-mod fortunes cowsay cowsay-off sl cmatrix cmatrix-xfont vlc geany geany-plugins qcomicbook
