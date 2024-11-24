7afd53a6af3e7ba6e09594d52bb540e7ab68e8eb
Matt Marino <mttym42@icloud.com>
2024-11-24 14:10:56 -0500

    arranging for installs

---
+++ deb_scripts/move.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+sudo cp -r /home/mattm/Downloads/Fonts/* /usr/share/fonts
+
+sudo cp -r /home/mattm/Downloads/icons/* /usr/share/icons
+
+sudo cp -r /home/mattm/Downloads/themes/* /usr/share/themes
+
+sudo cp -r /home/mattm/Pictures/backgrounds/* /usr/share/backgrounds
+
--- deb_scripts/repo.sh
+++ deb_scripts/repo.sh
@@ -2,14 +2,14 @@
 
 ## add multimedia repo - may need sudo
 
-echo -e 'deb https://www.deb-multimedia.org bookworm main non-free' >> /etc/apt/sources.list.d/multimedia.list
+echo -e 'deb https://www.deb-multimedia.org bookworm main non-free' | sudo tee -a /etc/apt/sources.list.d/multimedia.list
 
 #sudo nano /etc/apt/sources.list.d/multimedia.list
 #deb https://www.deb-multimedia.org bookworm main non-free
 
 ## add backports if not enabled during setup
 
-#echo -e 'deb https://debian.csail.mit.edu/debian bookworm-backports main' >> /etc/apt/sources.list.d/backports.list
+#echo -e 'deb https://debian.csail.mit.edu/debian bookworm-backports main' | sudo tee -a /etc/apt/sources.list.d/backports.list
 
 ## update system to recognize new repos
 
