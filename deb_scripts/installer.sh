3c747a87218ed0e05b549c93578d1c2218981c8a
Matt Marino <mttym42@icloud.com>
2024-11-24 22:08:31 -0500

    added installer script to deb_scripts

--- deb_scripts/installer.sh
+++ deb_scripts/installer.sh
@@ -1,13 +1,28 @@
+#! /bin/bash
+
+
 bash ~/Documents/deb_scripts/mediaring.sh
+
 bash ~/Documents/deb_scripts/repo.sh
+
 bash ~/Documents/deb_scripts/pipewire.sh
+
 bash ~/Documents/deb_scripts/package.sh
+
 bash ~/Documents/deb_scripts/dvd.sh
+
 bash ~/Documents/deb_scripts/nord.sh
+
 bash ~/Documents/deb_scripts/flatpak.sh
+
 bash ~/Documents/deb_scripts/brave.sh
+
 bash ~/Documents/deb_scripts/kitty.sh
+
 bash ~/Documents/deb_scripts/starship.sh
+
 bash ~/Documents/deb_scripts/gitcli.sh
+
 bash ~/Documents/deb_scripts/move.sh
+
 bash ~/Documents/deb_scripts/kicadsetup.sh
