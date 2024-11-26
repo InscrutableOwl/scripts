878048565ef6f9b6864d7166c3cff8157491e025
InscrutableOwl <148409977+InscrutableOwl@users.noreply.github.com>
2024-11-25 21:42:54 -0500

    Delete fed_scripts directory

--- fed_scripts/dnf.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-## Fedora packages
-
-sudo dnf install fastfetch lolcat innoextract htop lame dnfdragora dnfdragora-updater git curl timeshift fortune-mod cowsay sl cmatrix qcomicbook strawberry
--- fed_scripts/nordf.sh
+++
@@ -1,5 +0,0 @@
-sudo rpm -v --import https://repo.nordvpn.com/gpg/nordvpn_public.asc
-
-sudo dnf config-manager addrepo --id=nordvpn --set=name='NordVPN' --set=baseurl='https://repo.nordvpn.com/yum/nordvpn/centos/x86_64/'
-
-sudo dnf install nordvpn
