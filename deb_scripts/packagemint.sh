8af50e05da80b30e4f66b43aa6c91670b1424b3b
Matt Marino <mttym42@icloud.com>
2024-12-04 08:39:36 -0500

    edits to lmdesetup

--- deb_scripts/packagemint.sh
+++ deb_scripts/packagemint.sh
@@ -2,4 +2,4 @@
 
 ## Mint packages
 
-sudo apt install lolcat innoextract htop lame curl fortune-mod fortunes cowsay cowsay-off sl cmatrix cmatrix-xfont vlc qcomicbook strawberry cgns-convert wxmaxima qt5ct -y
+sudo apt install lolcat innoextract htop lame curl fortune-mod fortunes cowsay cowsay-off sl cmatrix cmatrix-xfont vlc qcomicbook strawberry cgns-convert wxmaxima qt5ct plank geany-plugin-git-changebar geany-plugin-spellcheck geany-plugin-treebrowser -y
