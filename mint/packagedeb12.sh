ab1f600cf1fdb6d55556ce4ceff2380a65196eeb
Matt Marino <mttym42@icloud.com>
2025-04-14 10:38:32 -0400

    removed lightdm-settings and slick-greeter from debian packages

--- mint/packagedeb12.sh
+++ mint/packagedeb12.sh
@@ -2,6 +2,6 @@
 
 ## Debian packages
 
-sudo apt install lolcat innoextract htop lame git curl fortune-mod fortunes cowsay cowsay-off sl cmatrix cmatrix-xfont vlc qcomicbook papirus-icon-theme bibata-cursor-theme plank qt5ct neofetch strawberry cgns-convert wxmaxima geany geany-plugin-addons geany-plugin-git-changebar geany-plugin-spellcheck geany-plugin-treebrowser lightdm-settings slick-greeter -y
+sudo apt install lolcat innoextract htop lame git curl fortune-mod fortunes cowsay cowsay-off sl cmatrix cmatrix-xfont vlc qcomicbook papirus-icon-theme bibata-cursor-theme plank qt5ct neofetch strawberry cgns-convert wxmaxima geany geany-plugin-addons geany-plugin-git-changebar geany-plugin-spellcheck geany-plugin-treebrowser -y
 
 sudo apt remove firefox-esr libreoffice-* -y
