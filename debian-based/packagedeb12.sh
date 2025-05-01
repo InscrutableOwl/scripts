a204ba77ae8007059acb14ea1f7eac51f097cbd2
Matt Marino <mttym42@icloud.com>
2025-05-01 08:26:07 -0400

    removing python/qtile packages

--- debian-based/packagedeb12.sh
+++ debian-based/packagedeb12.sh
@@ -2,6 +2,6 @@
 
 ## Debian packages
 
-sudo apt install lolcat innoextract htop lame git curl stow fortune-mod fortunes cowsay cowsay-off sl cmatrix cmatrix-xfont thunderbird vlc qcomicbook flatpak bibata-cursor-theme neofetch audacious cgns-convert wxmaxima sox libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev python3 python3-pip python3-venv python3-v-sim python-dbus-dev libpangocairo-1.0-0 python3-cairocffi python3-xcffib libxkbcommon-dev libxkbcommon-x11-dev alsa-utils cmake meson ninja-build pkg-config -y
+sudo apt install lolcat innoextract htop lame git curl stow fortune-mod fortunes cowsay cowsay-off sl cmatrix cmatrix-xfont thunderbird vlc qcomicbook flatpak bibata-cursor-theme neofetch audacious cgns-convert wxmaxima sox libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev cmake meson ninja-build pkg-config -y
 
 sudo apt remove firefox-esr libreoffice-* -y
