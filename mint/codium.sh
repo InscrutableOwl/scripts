bd645d2d203ce72a00d29565dc092a2dbd8448e0
InscrutableOwl <mttym42@icloud.com>
2025-05-01 13:10:24 -0400

    adding mint scripts

---
+++ mint/codium.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## VSCodium editor
+
+## add the GPG key of the repo
+
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+
+## add the repository
+
+echo 'deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg] https://download.vscodium.com/debs vscodium main' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.list
+
+## update and install codium
+
+sudo apt update && sudo apt install codium
---
+++ mint/installmint.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+## flor Linux Mint
+
+bash ~/scripts/mint/packagemint.sh
+
+bash ~/scripts/mint/dvd.sh
+
+bash ~/scripts/mint/codium.sh
+
+bash ~/scripts/mint/kitty.sh
+
+bash ~/scripts/mint/flatpakmint.sh
+
+bash ~/scripts/mint/move.sh
+
+bash ~/scripts/mint/starship.sh
+
+bash ~/scripts/mint/nerdfonts.sh
+
+bash ~/scripts/mint/gitcli.sh
+
+bash ~/scripts/mint/telaicon.sh
+
