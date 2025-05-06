372856e5ca07a4f9161601778bdcb3fa2a56d90b
Matt Marino <mttym42@icloud.com>
2025-05-06 07:34:08 -0400

    adding lmde scripts

--- debian-based/packagemint.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-## Mint packages
-
-sudo apt install lolcat innoextract htop stow lame curl fortune-mod fortunes cowsay cowsay-off sl cmatrix cmatrix-xfont rofi eza vlc qcomicbook audacious cgns-convert wxmaxima kicad cmake meson ninja-build pkg-config -y
-
---
+++ lmde/lmdes.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+##for LMDE
+
+bash ~/scripts/lmde/mediaring.sh
+bash ~/scripts/lmde/repo.sh
+bash ~/scripts/lmde/packagelmde.sh
+# bash ~/scripts/lmde/brave.sh
+bash ~/scripts/lmde/dvd.sh
+bash ~/scripts/lmde/kitty.sh
+bash ~/scripts/lmde/starship.sh
+bash ~/scripts/lmde/gitcli.sh
+bash ~/scripts/lmde/move.sh
+bash ~/scripts/lmde/flatpak.sh
+bash ~/scripts/lmde/nerdfonts.sh
+# bash ~/scripts/lmde/kicadsetup.sh
+bash ~/scripts/lmde/codium.sh
+bash ~/scripts/lmde/telaicon.sh
