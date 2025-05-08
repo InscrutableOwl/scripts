ad5dc5e837ea535b5628fcb5c0c6efc264682cbe
Matt Marino <mttym42@icloud.com>
2025-05-08 07:18:50 -0400

    adding new themes

--- mint/installmint.sh
+++ mint/installmint.sh
@@ -8,13 +8,13 @@ bash ~/scripts/mint/dvd.sh
 
 bash ~/scripts/mint/codium.sh
 
-bash ~/scripts/mint/kitty.sh
+# bash ~/scripts/mint/kitty.sh
 
 bash ~/scripts/mint/flatpakmint.sh
 
 bash ~/scripts/mint/move.sh
 
-bash ~/scripts/mint/starship.sh
+# bash ~/scripts/mint/starship.sh
 
 bash ~/scripts/mint/nerdfonts.sh
 
