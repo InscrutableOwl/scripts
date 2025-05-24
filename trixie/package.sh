ec397296244ac3ff9f5c0624876782d1d54b2cd7
Matt Marino <mttym42@icloud.com>
2025-05-24 09:50:54 -0400

    adding scripts for trixie

---
+++ trixie/package.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+## Mint packages
+
+sudo apt install lolcat innoextract htop stow lame curl fortune-mod fortunes git cowsay cowsay-off sl nala exa btop libdvdcss2 cmatrix cmatrix-xfont rofi vlc qcomicbook audacious cgns-convert wxmaxima cmake meson ninja-build pkg-config geany geany-plugin-addons geany-plugin-git-changebar geany-plugin-treebrowser -y
+
+
---
+++ trixie/telaicon.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+git clone https://github.com/vinceliuice/Tela-icon-theme.git
+
+cd Tela-icon-theme
+
+sudo ./install.sh -d /usr/share/icons blue green
+
