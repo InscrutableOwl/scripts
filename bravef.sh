aacdfd463a127012555fb45d1c19f7de6cdd10b8
Matt Marino <mttym42@icloud.com>
2025-05-30 11:46:58 -0400

    cleaning unused files adding backgrounds

--- bravef.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## Brave Browser
-
-sudo dnf install dnf-plugins-core
-
-sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-
-sudo dnf install brave-browser
--- dnfp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-## Fedora Packages
-
-sudo dnf install lolcat innoextract htop stow lame curl fortune-mod cowsay sl cmatrix qcomicbook
