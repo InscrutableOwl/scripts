ced5135e36298008764d9c4a2b2d41b4cc5301d0
InscrutableOwl <okami768@proton.me>
2026-02-06 21:51:49 -0500

    adding to dinst.sh

--- dinst.sh
+++ dinst.sh
@@ -6,6 +6,14 @@ sudo apt install lolcat innoextract git curl wget sl vlc htop btop cowsay cowsay
 
 sudo dpkg-reconfigure libdvd-pkg
 
+git clone https://github.com/InscrutableOwl/dotfiles.git ~/dotfiles
+
+sudo cp -r ~/dotfiles/backgrounds/* /usr/share/backgrounds
+
+bash ~/scripts/nerdfonts.sh
+
+
+
 
 
 
