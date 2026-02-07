f38cf24f190d2a35aeb92da1c32734e5f2ac1a4c
InscrutableOwl <okami768@proton.me>
2026-02-06 19:21:22 -0500

    updating dinst.sh

--- dinst.sh
+++ dinst.sh
@@ -2,7 +2,7 @@
 
 sudo apt update; sudo apt upgrade
 
-sudo apt install lolcat innoextract git curl wget sl vlc htop btop cowsay cowsay-off maxima wxmaxima stow fortune-mod fortunes regionset libdvd-pkg audacious colossal-cave-adventure nethack-console nethack-x11 fastfetch eza nala -y
+sudo apt install lolcat innoextract git curl wget sl vlc htop btop cowsay cowsay-off maxima wxmaxima stow fortune-mod fortunes regionset libdvd-pkg audacious fastfetch eza nala -y
 
 sudo dpkg-reconfigure libdvd-pkg
 
