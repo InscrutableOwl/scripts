5361b671f7d779f2ebcd340fe0bad71e4f7ad9c8
InscrutableOwl <okami768@proton.me>
2026-02-12 09:18:36 -0500

    adding scripts

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+bash ~/scripts/package.sh
+
+bash ~/scripts/dvd.sh
+
+bash ~/scripts/install_nerdfonts
+
+git clone https://github.com/InscrutableOwl/dotfiles.git ~/dotfiles
+
+bash ~/scripts/gitcli.sh
+
+bash~/scripts/protonvpn.sh
+
