06759f1d95b461032024c49cf4d42da5b9659948
InscrutableOwl <okami768@proton.me>
2026-02-12 11:30:45 -0500

    fixing typos

--- install.sh
+++ install.sh
@@ -6,9 +6,11 @@ bash ~/scripts/dvd.sh
 
 bash ~/scripts/install_nerdfonts
 
+bash ~/scripts/kitty.sh
+
 git clone https://github.com/InscrutableOwl/dotfiles.git ~/dotfiles
 
 bash ~/scripts/gitcli.sh
 
-bash~/scripts/protonvpn.sh
+bash ~/scripts/protonvpn.sh
 
