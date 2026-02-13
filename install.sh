16e2b97819eb10e9e4fb7283bd207e2ed0000e34
InscrutableOwl <okami768@proton.me>
2026-02-12 21:26:51 -0500

    adding qtile pkgs

--- install.sh
+++ install.sh
@@ -4,13 +4,17 @@ bash ~/scripts/package.sh
 
 bash ~/scripts/dvd.sh
 
-bash ~/scripts/install_nerdfonts
+bash ~/scripts/install_nerdfonts.sh
 
 bash ~/scripts/kitty.sh
+
+bash ~/scripts/qtilepkgs.sh
 
 git clone https://github.com/InscrutableOwl/dotfiles.git ~/dotfiles
 
 bash ~/scripts/gitcli.sh
+
+bash ~/scripts/install_picom.sh
 
 bash ~/scripts/protonvpn.sh
 
--- package.sh
+++ package.sh
@@ -2,7 +2,7 @@
 
 ## Mint packages
 
-sydo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade
 
 sudo apt install lolcat innoextract git htop stow lame curl fortune-mod fortunes cowsay sl btop cmatrix bat eza nala vlc cgns-convert wxmaxima mplayer mpv -y
 
