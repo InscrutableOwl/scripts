d0e667cf153c43b0a2dfefbf23c814f0b98d5bce
InscrutableOwl <148409977+InscrutableOwl@users.noreply.github.com>
2026-02-18 21:40:58 -0500

    Uncomment theme and font copy commands in theme.sh

--- theme.sh
+++ theme.sh
@@ -6,13 +6,13 @@ mkdir -p ~/.themes
 
 mkdir -p ~/.local/share/fonts
 
-# sudo cp -r ~/theming/themes/* /usr/share/themes
+sudo cp -r ~/theming/themes/* /usr/share/themes
 
 sudo cp -r ~/theming/backgrounds/* /usr/share/backgrounds
 
 sudo cp -r ~/theming/icons/* /usr/share/icons
 
-# cp -r ~/theming/themes/* ~/.themes
+cp -r ~/theming/themes/* ~/.themes
 
 cp -r ~/theming/fonts/* ~/.local/share/fonts
 
