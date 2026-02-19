26f77998f091a05b2c100810724e08a37587b84e
InscrutableOwl <okami768@proton.me>
2026-02-19 11:13:20 -0500

    adding XFCE themes

--- fedpkg.sh
+++ fedpkg.sh
@@ -4,5 +4,5 @@
 
 sudo dnf update
 
-sudo dnf install lolcat innoextract git htop stow lame curl fortune-mod cowsay sl btop cmatrix bat vlc wxMaxima maxima fastfetch -y
+sudo dnf install lolcat innoextract git htop stow lame curl fortune-mod cowsay sl btop cmatrix bat vlc wxMaxima maxima fastfetch tealdeer mplayer mpv rofi -y
 
--- finst.sh
+++ finst.sh
@@ -12,3 +12,14 @@ git clone https://github.com/InscrutableOwl/dotfiles.git ~/dotfiles
 
 bash ~/scripts/theme.sh
 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+sudo dnf upgrade --enablerepo=updates-testing --refresh --advisory=FEDORA-2026-8a21370dac
+
+rm ~/.bashrc
+
+cd ~/dotfiles
+
+stow bashrc ghostty sounds rofi
+
+cd
