9a5cb74ffe5da4db2d8796101229f2aac183e857
InscrutableOwl <okami768@proton.me>
2026-02-19 10:42:00 -0500

    adding KDE theming

--- theme.sh
+++ theme.sh
@@ -2,17 +2,15 @@
 
 git clone https://github.com/InscrutableOwl/theming.git ~/theming
 
-mkdir -p ~/.themes
+mkdir -p ~/.local/share/color-schemes
 
 mkdir -p ~/.local/share/fonts
-
-sudo cp -r ~/theming/themes/* /usr/share/themes
 
 sudo cp -r ~/theming/backgrounds/* /usr/share/backgrounds
 
 sudo cp -r ~/theming/icons/* /usr/share/icons
 
-cp -r ~/theming/themes/* ~/.themes
+cp -r ~/theming/themes/* ~/.local/share/color-schemes
 
 cp -r ~/theming/fonts/* ~/.local/share/fonts
 
