faa440e31860296d3bc9e84223327783b2b67d6d
InscrutableOwl <okami768@proton.me>
2026-02-19 14:42:24 -0500

    theme.sh

--- theme.sh
+++ theme.sh
@@ -4,17 +4,17 @@ git clone https://github.com/InscrutableOwl/theming.git ~/theming
 
 mkdir -p ~/.themes
 
-# mkdir -p ~/.local/share/fonts
+mkdir -p ~/.local/share/fonts
 
 sudo cp -r ~/theming/themes/* /usr/share/themes
 
 sudo cp -r ~/theming/backgrounds/* /usr/share/backgrounds
 
-# sudo cp -r ~/theming/icons/* /usr/share/icons
+sudo cp -r ~/theming/icons/* /usr/share/icons
 
 cp -r ~/theming/themes/* ~/.themes
 
-# cp -r ~/theming/fonts/* ~/.local/share/fonts
+cp -r ~/theming/fonts/* ~/.local/share/fonts
 
 cp -r ~/theming/avatars ~/Pictures
 
