1b9df42991d53d795b620b81c71b4d961a44567d
InscrutableOwl <okami768@proton.me>
2026-02-24 17:57:52 -0500

    cleaning scripts

--- wayfire/2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/.config/wayfire
-cp /etc/xdg/wayfire.ini ~/.config/wayfire.ini
--- wayfire/5.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo apt install xdg-desktop-portal-wlr
-
-echo MOZ_ENABLE_WAYLAND=1 >> ~/.profile
