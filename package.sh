a99fadac2148e4a9a12dab78828f9d5f7565509c
InscrutableOwl <148409977+InscrutableOwl@users.noreply.github.com>
2026-02-25 09:09:03 -0500

    Add Butterrepo and install Ghostty and Zen Browser
    Added commands to install Ghostty and Zen Browser from Butterrepo.

--- package.sh
+++ package.sh
@@ -6,3 +6,10 @@ sudo apt update && sudo apt upgrade
 
 sudo apt install lolcat innoextract git htop stow flatpak lame curl fortune-mod fortunes cowsay sl btop cmatrix bat eza nala vlc cgns-convert wxmaxima mplayer mpv papirus-icon-theme bibata-cursor-theme ani-cli lightdm-settings slick-greeter -y
 
+curl -fsSL https://justaguylinux.codeberg.page/butterrepo/key.asc | sudo gpg --dearmor -o /usr/share/keyrings/butterrepo.gpg
+
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/butterrepo.gpg] https://justaguylinux.codeberg.page/butterrepo stable main" | sudo tee /etc/apt/sources.list.d/butterrepo.list
+
+sudo apt update
+
+sudo apt install ghostty zen-browser
