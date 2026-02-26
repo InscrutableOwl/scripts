190001b08ef318e2fcf677459e08a7980b1d72d0
InscrutableOwl <148409977+InscrutableOwl@users.noreply.github.com>
2026-02-26 14:00:02 -0500

    Update package installation in package.sh
    Removed 'lightdm-settings' and 'slick-greeter' from installation.

--- package.sh
+++ package.sh
@@ -4,4 +4,4 @@
 
 sudo apt update && sudo apt upgrade
 
-sudo apt install lolcat innoextract git htop stow flatpak lame curl fortune-mod fortunes cowsay sl btop cmatrix bat eza nala vlc cgns-convert wxmaxima mplayer mpv papirus-icon-theme bibata-cursor-theme ani-cli lightdm-settings slick-greeter fastfetch -y
+sudo apt install lolcat innoextract git htop stow flatpak lame curl fortune-mod fortunes cowsay sl btop cmatrix bat eza nala vlc cgns-convert wxmaxima mplayer mpv papirus-icon-theme bibata-cursor-theme ani-cli fastfetch -y
