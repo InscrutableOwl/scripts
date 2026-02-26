aab1581820d27b6b40276824192c1db6b4bbdb65
InscrutableOwl <okami768@proton.me>
2026-02-26 16:39:50 -0500

    removing fedora scripts

--- fedpkg.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-## Fedora packages
-
-sudo dnf update
-
-sudo dnf install lolcat innoextract git htop stow lame curl fortune-mod cowsay sl btop cmatrix bat vlc wxMaxima maxima fastfetch tealdeer mplayer mpv kitty -y
-
--- move.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-sudo cp -r ~/dotfiles/fonts/* ~/.fonts
-sudo cp -r ~/dotfiles/icons/* ~/.icons
-sudo cp -r ~/dotfiles/themes/* ~/.themes
-cp -r ~/dotfiles/Pictures/* ~/Pictures
-# mkdir -p ~/Games
-# mv ~/scripts/Roms/* ~/Games
