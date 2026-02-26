7f6eccf4de8ff18b796d62c3c0808003720df17f
InscrutableOwl <148409977+InscrutableOwl@users.noreply.github.com>
2026-02-26 10:47:05 -0500

    Uncomment yazi installation commands in copr.sh

--- copr.sh
+++ copr.sh
@@ -3,8 +3,8 @@
 sudo dnf copr enable derisis13/ani-cli -y
 sudo dnf install -y ani-cli
 
-# sudo dnf copr enable lihaohong/yazi
-# sudo dnf install -y yazi
+sudo dnf copr enable lihaohong/yazi -y
+sudo dnf install -y yazi
 
 # sudo dnf copr enable scottames/ghostty
 # sudo dnf install -y ghostty
