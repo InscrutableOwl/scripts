7cfe90cd1635df8a0686b719d23d4255bb973358
InscrutableOwl <okami768@proton.me>
2026-02-28 15:20:40 -0500

    modifying scripts

--- brave.sh
+++ brave.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+# sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
-sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+# sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
 
-sudo apt update
+# sudo apt update
 
-sudo apt install brave-browser -y
+# sudo apt install brave-browser -y
 
-# sudo dnf install dnf-plugins-core -y
+sudo dnf install dnf-plugins-core -y
 
-# sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 
-# sudo dnf install brave-browser -y
+sudo dnf install brave-browser -y
--- rpmf.sh
+++ rpmf.sh
@@ -4,9 +4,9 @@ sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-releas
 
 sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
 
-sudo dnf update @core
+sudo dnf update @core -y
 
-sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
 
 sudo dnf install rpmfusion-free-release-tainted -y
 
