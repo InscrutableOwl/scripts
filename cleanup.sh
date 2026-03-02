78266b7629754f6d8c2ef8edef7e08167c716447
InscrutableOwl <148409977+InscrutableOwl@users.noreply.github.com>
2026-03-01 20:31:23 -0500

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -7,3 +7,13 @@ cd ~/dotfiles
 stow bashrc sounds alacritty
 
 cd
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo >> /home/mattm/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"' >> /home/mattm/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
+
+brew install gcc
+
+brew install tealdeer
