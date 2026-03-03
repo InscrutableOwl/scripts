4e7572c67186b8f4f5a8b6f9549d1ccd7392e3a7
InscrutableOwl <148409977+InscrutableOwl@users.noreply.github.com>
2026-03-03 15:41:13 -0500

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -11,7 +11,7 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 
 ## Betterbird
 
-#flatpak install flathub eu.betterbird.Betterbird -y
+flatpak install flathub eu.betterbird.Betterbird -y
 
 ## Krita
 
@@ -35,7 +35,7 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 
 ## Zen Browser
 
-#flatpak install flathub app.zen_browser.zen
+flatpak install flathub app.zen_browser.zen
 
 ## add others as needed ##
 
