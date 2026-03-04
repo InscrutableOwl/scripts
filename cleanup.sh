8a3a486d9172af765782683f07fc7de24e7ef855
InscrutableOwl <okami768@proton.me>
2026-03-04 13:56:53 -0500

    ubuntu commits

--- cleanup.sh
+++ cleanup.sh
@@ -10,4 +10,4 @@ git clone https://github.com/InscrutableOwl/dotfiles.git ~/dotfiles
 
 #cd
 
-#sudo dnf remove firefox -y
+sudo snap remove firefox thunderbird -y
---
+++ ubuntu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+bash ~/scripts/package.sh
+bash ~/scripts/dvd.sh
+bash ~/scripts/protonvpn.sh
+bash ~/scripts/cleanup.sh
