26bb59e310d72709f248a0c1ad5aac3486361a82
InscrutableOwl <okami768@proton.me>
2026-03-04 10:02:17 -0500

    cleaning fedora scripts

--- protonvpn.sh
+++ protonvpn.sh
@@ -8,6 +8,6 @@
 
 wget "https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.3-1.noarch.rpm"
 
-sudo dnf install ./protonvpn-stable-release-1.0.3-1.noarch.rpm && sudo dnf check-update --refresh 
+sudo dnf install ./protonvpn-stable-release-1.0.3-1.noarch.rpm && sudo dnf check-update --refresh -y 
 
-sudo dnf install proton-vpn-cli -y
+sudo dnf install proton-vpn-cli pipx -y
