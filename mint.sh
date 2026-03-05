3e518bf81250d766561b4f195658adbe513cee7b
InscrutableOwl <okami768@proton.me>
2026-03-04 21:23:56 -0500

    adding install scripts for mint

--- mint.sh
+++ mint.sh
@@ -3,4 +3,4 @@
 bash ~/scripts/package.sh
 bash ~/scripts/dvd.sh
 bash ~/scripts/protonvpndeb.sh
-bash ~/scripts/cleanup.sh
+bash ~/scripts/mintcleanup.sh
---
+++ mintcleanup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone https://github.com/InscrutableOwl/dotfiles.git ~/dotfiles
+
+#rm ~/.bashrc
+
+#cd ~/dotfiles
+
+#stow bashrc sounds alacritty rofi
+
+#cd
+
+sudo apt remove firefox thunderbird -y
