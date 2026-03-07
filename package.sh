e22bf83e1a0e81728398af13b60d8457396f540d
InscrutableOwl <148409977+InscrutableOwl@users.noreply.github.com>
2026-03-07 17:00:32 -0500

    Update package.sh

--- package.sh
+++ package.sh
@@ -2,4 +2,4 @@
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install lolcat innoextract eza nala alacritty btop fastfetch sl cowsay papirus-icon-theme bibata-cursor-theme bat wxmaxima maxima mplayer mpv vlc gcc make build-essential cmatrix fortunes fortune-mod ani-cli lame curl wget stow cgns-convert rofi flatpak -y
+sudo apt install lolcat innoextract eza nala alacritty btop fastfetch sl cowsay papirus-icon-theme bibata-cursor-theme bat wxmaxima maxima mplayer mpv vlc gcc make build-essential cmatrix fortunes fortune-mod ani-cli lame curl wget stow cgns-convert -y
