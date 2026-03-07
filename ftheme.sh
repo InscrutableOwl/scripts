a266e69d91eea481f325c7e6faa85f3451041f89
InscrutableOwl <148409977+InscrutableOwl@users.noreply.github.com>
2026-03-06 20:10:18 -0500

    Update ftheme.sh

--- ftheme.sh
+++ ftheme.sh
@@ -2,9 +2,9 @@
 
 git clone https://github.com/InscrutableOwl/theming.git ~/theming
 
-mkdir -p ~/.themes
+#mkdir -p ~/.themes
 
-sudo cp -r ~/theming/themes/* /usr/share/themes
+#sudo cp -r ~/theming/themes/* /usr/share/themes
 
 sudo cp -r ~/theming/backgrounds/* /usr/share/backgrounds
 
@@ -12,7 +12,7 @@ sudo cp -r ~/theming/fonts/* /usr/share/fonts
 
 sudo cp -r ~/theming/icons/* /usr/share/icons
 
-cp  ~/theming/themes/* ~/.themes
+#cp  ~/theming/themes/* ~/.themes
 
 cp -r ~/theming/avatars ~/Pictures
 
