de7938a6fac4967f3c7e21a8a234133a940d8c5a
InscrutableOwl <okami768@proton.me>
2026-04-13 08:09:54 -0400

    initial commit

---
+++ package.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install lolcat sl cowsay fortunes fortune-mod ani-cli mpv mplayer aisleriot gnome-cards-data innoextract build-essential gcc make git asunder eza nala fastfetch btop vlc cmatrix lame curl wget stow toilet figlet innoextract bat -y
+
+sudo apt install libdvd-pkg regionset -y
+sudo dpkg-reconfigure libdvd-pkg 
+
+wget https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.8_all.deb
+
+sudo dpkg -i ./protonvpn-stable-release_1.0.8_all.deb && sudo apt update -y
+
+sudo apt install proton-vpn-cli
+#sudo apt install proton-vpn-gnome-desktop
+
+sudo apt remove peg-e swell-foop -y
