4649af8b58408b73957d26d0979db9904c088e1e
InscrutableOwl <148409977+InscrutableOwl@users.noreply.github.com>
2026-08-03 07:07:43 -0400

    Modify package installation and theme setup in script
    Removed installation of some packages and commented out theme-related commands.

--- packagefed.sh
+++ packagefed.sh
@@ -12,13 +12,13 @@ sudo dnf install rpmfusion-free-release-tainted
 
 sudo dnf install libdvdcss
 
-sudo dnf install lolcat sl cowsay mpv mplayer aisleriot innoextract gcc make git fastfetch btop vlc lame curl wget stow toilet figlet bat gnome-tweaks -y
+sudo dnf install lolcat sl cowsay mpv mplayer innoextract gcc make git fastfetch btop vlc lame curl wget stow toilet figlet bat -y
 
 wget "https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.4-1.noarch.rpm"
 
 sudo dnf install ./protonvpn-stable-release-1.0.4-1.noarch.rpm && sudo dnf check-update --refresh 
 
-sudo dnf install proton-vpn-gnome-desktop
+sudo dnf install proton-vpn-cli
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
@@ -26,16 +26,16 @@ sudo dnf copr enable derisis13/ani-cli
 
 sudo dnf install ani-cli
 
-mkdir -p ~/.themes
+#mkdir -p ~/.themes
 
 git clone https://github.com/InscrutableOwl/theming.git ~/theming
 
-sudo cp -r ~/theming/themes/* /usr/share/themes
+#sudo cp -r ~/theming/themes/* /usr/share/themes
 
 sudo cp -r ~/theming/fonts/* /usr/share/fonts
 
 sudo cp -r ~/theming/icons/* /usr/share/icons
 
-cp -r ~/theming/themes/* ~/.themes
+#cp -r ~/theming/themes/* ~/.themes
 
-flatpak install flathub com.mattjakeman.ExtensionManager
+#flatpak install flathub com.mattjakeman.ExtensionManager
